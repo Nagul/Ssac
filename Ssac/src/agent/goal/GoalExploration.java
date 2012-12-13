bdfa0c947cd6c629e1c32c07c4a779d8b0cfbcc3
Rhum1 <romain.bobo312@gmail.com>
2012-12-13 01:50:40 +0100

    Algo d'exploration bete : tout droit vers la destination, puis astar dès l'obstacle en mode godlike A améliorer : A star jusqu'au point connu le plus proche de la destination, puis tout droit, puis cheat mode pour l'obstacle Terrain modifié pour créer une map partielle (cool), et pour une map vierge pour astar tout droit (sale, mais bon)

--- Ssac/src/agent/goal/GoalExploration.java
+++ Ssac/src/agent/goal/GoalExploration.java
@@ -1,9 +1,11 @@
 package agent.goal;
 
 import agent.agents.Agent;
+import agent.raisonnement.MoveAction;
+import environnement.Coordonnee;
 
 public class GoalExploration extends Goal {
-
+	
 	public GoalExploration(Agent a) {
 		super(a);
 	}
@@ -11,11 +13,13 @@ public class GoalExploration extends Goal {
 	public void calculGoal() {
 		priorite = 20;
 	}
-
+	
 	@Override
 	public void createPlanning() {
-		// TODO Auto-generated method stub
-		
+		Goal task = agent.getGoals().get(0);
+		Coordonnee destination = new Coordonnee(agent.getCoordonnee().getAbscisse(), agent.getCoordonnee().getOrdonnee() + 100);
+		MoveAction action = new MoveAction(agent, task, destination);
+		this.agent.addToPlanning(action);
 	}
 	
 }
--- Ssac/src/agent/raisonnement/MoveAction.java
+++ Ssac/src/agent/raisonnement/MoveAction.java
@@ -1,48 +1,70 @@
 package agent.raisonnement;
 
+import pathFinding.AStarPathFinder;
 import pathFinding.Path;
+import affichage.TestQt;
 import agent.agents.Agent;
+import agent.goal.GoalExploration;
 import environnement.Coordonnee;
+import environnement.Terrain;
 import environnement.TypeObject;
 import environnement.TypeSmallObject;
+import environnement.TypeTerrain;
 
 public class MoveAction extends Action {
 	
-	private Coordonnee target ;
-	private Path path ;
-	private int index ;
+	private Coordonnee	target;
+	private Path		path;
+	private int			index;
 	
 	// On peut fournir une coordonnéee cible...
 	public MoveAction(Agent master, Task parent, Coordonnee target) {
-		super(master,parent) ;
-		this.target = target ;
-		this.path = null ;
-		index = 1 ;
+		super(master, parent);
+		this.target = target;
+		this.path = null;
+		index = 1;
 	}
 	
 	//	... ou un type d'objet à atteindre
 	public MoveAction(Agent master, Task parent, TypeObject type) {
-		this(master, parent, master.find(type)) ;
+		this(master, parent, master.find(type));
 	}
 	
 	public MoveAction(Agent master, Task parent, TypeSmallObject type) {
-		this(master, parent, master.find(type)) ;
+		this(master, parent, master.find(type));
 	}
 	
 	public boolean useful() {
-		checkPath() ;
-		return (path.getLength() == 1) ;
+		checkPath();
+		return (path.getLength() == 1);
 	}
 	
 	public void step() {
-		checkPath() ;
-		if(index < path.getLength()) {
-			master.moveTo(path.getCoordonnee(index)) ;
-			master.delay(1) ;
+		checkPath();
+		if (index < path.getLength()) {
+			// check de si je vais entrer dans un truc pas de la terre ou pas
+			Coordonnee dest = path.getCoordonnee(index);
+			// terre : ok
+			if (TestQt.environnement.getTerrain().getCase(dest).getType().equals(TypeTerrain.Terre)) {
+				master.moveTo(dest);
+				master.delay(1);
+			}
+			// pas terre et pas la destination (sinon on va boucler sur cette �tape) : on regenere le path
+			else if (index <= path.getLength() - 1) {
+				AStarPathFinder aStar = new AStarPathFinder(TestQt.environnement.getTerrain(), Integer.MAX_VALUE, false);
+				Path newPath = aStar.findPath(master, master.getCoordonnee().getAbscisse(), master.getCoordonnee().getOrdonnee(), target.getAbscisse(),
+						target.getOrdonnee());
+				
+				index = 1;
+				dest = path.getCoordonnee(index);
+				path = newPath;
+				master.moveTo(dest);
+				master.delay(1);
+			}
 		}
-		index += 1 ;
-		if(index >= path.getLength()) {
-			parent.terminate(this,true) ;
+		index += 1;
+		if (index >= path.getLength()) {
+			parent.terminate(this, true);
 		}
 	}
 	
@@ -52,11 +74,25 @@ public class MoveAction extends Action {
 	}
 	
 	private void checkPath() {
-		if(path == null) {
-			// Generate the path !
+		// Generate the path !
+		if (path == null) {
+			// explo : blind mode, tout schuss
+			if (this.parent instanceof GoalExploration) {
+				AStarPathFinder aStar = new AStarPathFinder(new Terrain(true), Integer.MAX_VALUE, false);
+				Path newPath = aStar.findPath(master, master.getCoordonnee().getAbscisse(), master.getCoordonnee().getOrdonnee(), target.getAbscisse(),
+						target.getOrdonnee());
+				path = newPath;
+			}
+			// sinon : cheat mode, carte static totale
+			else {
+				AStarPathFinder aStar = new AStarPathFinder(TestQt.environnement.getTerrain(), Integer.MAX_VALUE, false);
+				Path newPath = aStar.findPath(master, master.getCoordonnee().getAbscisse(), master.getCoordonnee().getOrdonnee(), target.getAbscisse(),
+						target.getOrdonnee());
+				path = newPath;
+			}
 		}
 	}
-
+	
 	@Override
 	public void terminate(Action child, boolean success) {
 		// TODO Auto-generated method stub
